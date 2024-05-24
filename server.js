ce219be950988d98acf393747c4438e0441a4a4f
gando537 <gandohd@gmail.com>
2024-05-24 10:21:07 +0200

    fixed

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Utiliser les routes
 app.use('/', index);
-app.use('/routes', router);
+app.use('/', router);
 
 // Lancement du serveur ---------------------------------------------------------------------
 app.listen(port, () => {
